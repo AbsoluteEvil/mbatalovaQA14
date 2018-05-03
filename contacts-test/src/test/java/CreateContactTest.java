dd4f948ee0596dc9c787e275e2de8d9587eead5a
AbsoluteEvil <37809997+AbsoluteEvil@users.noreply.github.com>
2018-05-03 11:54:01 +0200

    homework 2

---
+++ contacts-test/src/test/java/CreateContactTest.java
@@ -0,0 +1,69 @@
+//importLib
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CreateContactTest {
+ //MakeDriver
+  private WebDriver driver;
+
+  @BeforeClass(alwaysRun = true)
+  //beforeEveryTestOpenChromeDriver
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testCreateContact() throws Exception {
+    //openSite
+    driver.get("http://localhost/addressbook/");
+    //login
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.id("LoginForm")).submit();
+    //goToAddPage
+    driver.findElement(By.linkText("add new")).click();
+    //fillFirstname
+    driver.findElement(By.name("firstname")).clear();
+    driver.findElement(By.name("firstname")).sendKeys("Maria");
+    //fillLastname
+    driver.findElement(By.name("lastname")).clear();
+    driver.findElement(By.name("lastname")).sendKeys("Batalova");
+    //submit
+    driver.findElement(By.name("submit")).click();
+   }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    //checkThatElementWasAdded
+    driver.findElement(By.linkText("home page")).click();
+  }
+/* For future projects
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+  */
+}
